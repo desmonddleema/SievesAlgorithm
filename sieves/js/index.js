6c90bcea7eff67e5a456dc573f455037a4426c12
Demetra211 <desmonddleema211@gmail.com>
2021-05-13 14:20:43 +0530

    Implemented Sieve of Eratosthenes

--- sieves/js/index.js
+++ sieves/js/index.js
@@ -7,7 +7,9 @@ function generateNumbers() {
     for (let i = lower; i <= upper; i++) {
       text += '<div class="flex-child" id="child'+i+'"><p>' + i + "</p></div>";
     }
-    text += '</div><div class="buttonClass"><button onClick="even()">Even</button><button onClick="odd()">Odd</button><button onClick="reset()">Reset</button></div>';
+    // text += '</div><div class="buttonClass"><button onClick="even()">Even</button><button onClick="odd()">Odd</button><button onClick="sieve()">Prime</button><button onClick="reset()">Reset</button></div>';
+    
+    text += '</div><div class="buttonClass"><button Onclick="sieve()">Prime</button><button onClick="reset()">Reset</button></div>'
     document.getElementById("demo").innerHTML = text;
   }
 function even(){
@@ -16,7 +18,7 @@ function even(){
     for(let i=lower; i<=upper; i++){
         if(i%2 == 0){
             var value="child"+i;
-            document.getElementById(value).style.background="blue";
+            document.getElementById(value).style.background="orange";
         }
     }
 }
@@ -26,18 +28,32 @@ function odd(){
     for(let i=lower; i<=upper; i++){
         if(i%2 != 0){
             var value="child"+i;
-            document.getElementById(value).style.background="green";
+            document.getElementById(value).style.background="yellow";
         }
     }
 }
 function reset(){
     const lower = parseInt(document.getElementById("value1").value);
     const upper = parseInt(document.getElementById("value2").value);
+    document.getElementById("message").style.display = "none";
     for(let i=lower; i<=upper; i++){
-          var value="child"+i;
-         // console.log(value);
+        var value="child"+i;
         document.getElementById(value).style.background="wheat";
     }
 }
-
+function sieve(){
+    const lower = parseInt(document.getElementById("value1").value);
+    const upper = parseInt(document.getElementById("value2").value);
+    for(let i=2; i<=(upper/4); i++){
+        var value1 = i*i;
+        var inc = value1/i;
+        while(value1 <= upper){
+            var result = "child" + value1;
+            document.getElementById(result).style.background ="orange";
+            inc += 1;
+            value1 = i*inc;
+        }
+    document.getElementById("message").style.display = "block";
+    }
+}
   
